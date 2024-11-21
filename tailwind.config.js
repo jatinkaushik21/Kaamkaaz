39238973c707be36ee4e732b1b743043e2f16d18
jatinkaushik21 <jatinkaushik598@gmail,com>
2024-11-21 12:18:12 +0530

    Second

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 
 export default {
   content: [
-    "./public/index.html",                // Main HTML file
-    "./src/HelpCenter/Overview.html",               
+    "./index.html",              
     "./src/**/*.{html,js,jsx}",    // Include all HTML and JS files in /src
   ],
   theme: {
