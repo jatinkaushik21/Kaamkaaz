e4ba2906bac236978ef0f20d1235b6fb9df57aa2
jatinkaushik21 <jatinkaushik598@gmail,com>
2024-12-21 11:32:44 +0530

    spinfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
     "./src/**/*.{html,js,jsx}",    // Include all HTML and JS files in /src
   ],
   theme: {
-    extend: {},
+    extend: {
+       animation: {
+        bounceSlow: 'bounce 2s ease-in-out infinite',  // Custom slow bounce
+        spinSlow: 'spin 4s linear infinite', // Set to a slower duration, 
+      },
+    },
   },
   plugins: [],
 };
